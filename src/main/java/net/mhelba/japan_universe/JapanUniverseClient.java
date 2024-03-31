1f805c822cab619812eee0d7dc7ccb3d19bc5af6
mHelba <william.chasson@gmail.com>
2024-03-31 11:35:28 +0200

    Initial commit

---
+++ src/main/java/net/mhelba/japan_universe/JapanUniverseClient.java
@@ -0,0 +1,11 @@
+package net.mhelba.japan_universe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JapanUniverseClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
---
+++ src/main/java/net/mhelba/japan_universe/JapanUniverseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mhelba.japan_universe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JapanUniverseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
