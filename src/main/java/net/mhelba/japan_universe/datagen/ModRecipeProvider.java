77d2d8ec3b07e3b01844a005017082f83910e054
mHelba <william.chasson@gmail.com>
2024-03-31 22:04:15 +0200

    feat: add onigiri salmon recipe

--- src/main/java/net/mhelba/japan_universe/datagen/ModRecipeProvider.java
+++ src/main/java/net/mhelba/japan_universe/datagen/ModRecipeProvider.java
@@ -2,7 +2,12 @@ package net.mhelba.japan_universe.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.mhelba.japan_universe.item.ModItems;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
   public ModRecipeProvider(FabricDataOutput output) {
@@ -10,5 +15,24 @@ public class ModRecipeProvider extends FabricRecipeProvider {
   }
 
   @Override
-  public void generate(RecipeExporter exporter) {}
+  public void generate(RecipeExporter exporter) {
+    ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.ONIGIRI_SALMON, 8)
+        .pattern(" R ")
+        .pattern("RSR")
+        .pattern("RKR")
+        .input('R', ModItems.RICE_COOKED)
+        .input('S', Items.COOKED_SALMON)
+        .input('K', Items.DRIED_KELP)
+        .criterion(
+            ModRecipeProvider.hasItem(ModItems.RICE_COOKED),
+            ModRecipeProvider.conditionsFromItem(ModItems.RICE_COOKED))
+        .criterion(
+            ModRecipeProvider.hasItem(Items.COOKED_SALMON),
+            ModRecipeProvider.conditionsFromItem(Items.COOKED_SALMON))
+        .criterion(
+            ModRecipeProvider.hasItem(Items.DRIED_KELP),
+            ModRecipeProvider.conditionsFromItem(Items.DRIED_KELP))
+        .offerTo(
+            exporter, new Identifier(ModRecipeProvider.getRecipeName(ModItems.ONIGIRI_SALMON)));
+  }
 }
